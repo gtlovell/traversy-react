f87021694ede90a63ff170eaa368be335a5f043c
Garrett Lovell <seanhelmes@Garretts-MacBook-Pro-2.local>
2018-07-23 17:05:24 -0400

    form work

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -12,7 +12,7 @@ const Header = props => {
         <div>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <a href="#" className="nav-link">
+              <a href="/" className="nav-link">
                 Home
               </a>
             </li>
