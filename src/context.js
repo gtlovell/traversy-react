69f7fdfcf4ebb8334f96e684d251dee69fb45bb2
Garrett Lovell <seanhelmes@Garretts-MacBook-Pro-2.local>
2018-07-23 15:49:12 -0400

    first commit

---
+++ src/context.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+const Context = React.createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'DELETE_CONTACT':
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      };
+    default:
+      return state;
+  }
+};
+
+export class Provider extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: 'John Doe',
+        email: 'jdoe@email.com',
+        phone: '555-555-5555'
+      },
+      {
+        id: 2,
+        name: 'Lucy Smith',
+        email: 'lsmith@email.com',
+        phone: '777-777-7777'
+      },
+      {
+        id: 3,
+        name: 'James Williams',
+        email: 'jwilliams@email.com',
+        phone: '444-444-4444'
+      }
+    ],
+    dispatch: action => this.setState(state => reducer(state, action))
+  };
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export const Consumer = Context.Consumer;
